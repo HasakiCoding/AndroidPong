10db4fc34cbc17a87cdb4ce3aae23a642af88ebd
HasakiCoding <nils.koenig01@gmail.com>
2017-08-15 12:31:01 +0200

    overwrite 'game' and 'gameview'

--- app/src/main/java/pong/hasaki/com/androidpong/Game.java
+++ app/src/main/java/pong/hasaki/com/androidpong/Game.java
@@ -1,15 +1,38 @@
 package pong.hasaki.com.androidpong;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View.OnTouchListener;
+import android.view.View;
 
 public class Game extends Activity {
 
-    GameView GV;
+    GameView v;
+    //Bitmap ball;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GV = new GameView(this);
-        setContentView(GV);
+        v = new GameView(this);
+        //ball = BitmapFactory.decodeResource(getResources(), R.drawable.background_1);
+        setContentView(v);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        v.pause();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        v.resume();
     }
 }
--- app/src/main/java/pong/hasaki/com/androidpong/GameView.java
+++ app/src/main/java/pong/hasaki/com/androidpong/GameView.java
@@ -4,57 +4,95 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
-public class GameView extends View {
+public class GameView extends SurfaceView implements Runnable {
 
-    int paddle1Y = 0;
-    static int width = 20, height = 300;
-    static float radius = 30;
-    float ballX, ballY, eventY = 0;
+    Thread t = null;
+    SurfaceHolder holder;
+    boolean started = false, setup = false;
+    Canvas canvas;
+
     Rectangle field;
     Paddle paddle1;
     Paddle paddle2;
     Ball gameball;
 
+    static int width = 20, height = 300;
+    static float radius = 30;
 
     public GameView(Context context) {
         super(context);
+        holder = getHolder();
+        this.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent me) {
+                switch(me.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        paddle1.centerY = me.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        paddle1.centerY = me.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        paddle1.centerY = me.getY();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    public void run() {
+        while(started){
+            if(!holder.getSurface().isValid()){
+                continue;
+            }
+            canvas = holder.lockCanvas();
+            if(!setup) {
+                init(canvas);
+                setup = true;
+            }
+            //c.drawBitmap(ball, x-(ball.getWidth()/2), y-(ball.getHeight()/2), blue);
+            gameRender(canvas);
+            gameball.move(10, 10);
 
-        ballX = canvas.getWidth() / 2 - radius;
-        ballY = canvas.getHeight() / 2 - radius;
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
 
+    public void pause(){
+        started = false;
+        while(true){
+            try {
+                t.join();
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            break;
+        }
+        t = null;
+    }
+
+    public void resume(){
+        started = true;
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void init(Canvas canvas){
         field = new Rectangle(Color.BLACK, 0, 0, canvas.getWidth(), canvas.getHeight(), 1);
-        paddle1 = new Paddle(Color.WHITE, 10, paddle1Y, width, height);
+        paddle1 = new Paddle(Color.WHITE, 10, canvas.getHeight() / 2, width, height);
         paddle2 = new Paddle(Color.WHITE, canvas.getWidth() - 10, canvas.getHeight() / 2, width, height);
-        gameball = new Ball(Color.WHITE, ballX, ballY, radius);
+        gameball = new Ball(Color.WHITE, canvas.getWidth() / 2 - radius, canvas.getHeight() / 2 - radius, radius);
+    }
 
-        paddle1Y = Math.round(eventY);
-
+    public void gameRender(Canvas canvas){
         field.render(canvas);
         paddle1.render(canvas);
         paddle2.render(canvas);
         gameball.render(canvas);
-    }
-
-    public boolean onTouchEvent(MotionEvent event) {
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                paddle1Y = Math.round(event.getY());
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                paddle1Y = Math.round(event.getY());
-                break;
-            default:
-                return false;
-        }
-        invalidate();
-        return false;
     }
 }
